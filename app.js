143709759be977b63cce80879bd834ecdc9582a7
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2021-05-26 08:22:58 -0400

    set up server

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 
 const app = express()
+
+const port = process.env.PORT
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`)
+})
