da40b3519975a0b971c9b067d4aee3fc7c49301f
Mikey Nichols <mikenichols410@gmail.com>
2021-06-13 21:58:28 -0400

    add / to path to try and get path to point to right place

--- server/app.js
+++ server/app.js
@@ -38,9 +38,9 @@ const app = express()
 
 if ( process.env.NODE_ENV === 'production') {
     app.use(enforce.HTTPS({ trustProtoHeader: true}))
-    app.use(express.static(path.join(__dirname, 'client/build')))
+    app.use(express.static(path.join(__dirname, '/client/build')))
     app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+        res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
     })
 }
 
